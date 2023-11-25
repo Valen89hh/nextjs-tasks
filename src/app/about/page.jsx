1be39077c7a90a6a25323f852b73d357bc07f03f
Valen <126835108+Valen89hh@users.noreply.github.com>
2023-11-25 08:26:28 -0500

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return ( 
+        <section className="container mx-auto">
+            <h3 className="text-4xl font-bold my-5">About</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dolorum quos numquam reiciendis eum beatae incidunt voluptas eos itaque, possimus cumque dicta voluptates quo molestias? Consectetur doloribus sequi nobis dolores, quam tempora, architecto inventore voluptatibus saepe aut ipsa? Explicabo ullam fugiat saepe odit nemo eos at accusantium! Et iusto reprehenderit voluptatem error assumenda molestiae esse, consequatur fugiat labore quos facilis unde provident omnis ipsum consequuntur libero nobis explicabo sit minima? A impedit obcaecati alias cumque quisquam qui quae animi doloremque totam numquam, repudiandae incidunt veniam, ut commodi facere. Sed explicabo, inventore minima blanditiis deleniti eum aspernatur! Porro labore minima a?</p>
+        </section>
+     );
+}
+
+export default About;
